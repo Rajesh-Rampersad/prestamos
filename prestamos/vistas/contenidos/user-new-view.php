687b62d3e4d1de98a6db539898ad8dce5afe9a57
Daniel <danigarcia.negocio@gmail.com>
2023-07-24 16:53:04 -0300

    revision

--- prestamos/vistas/contenidos/user-new-view.php
+++ prestamos/vistas/contenidos/user-new-view.php
@@ -3,26 +3,30 @@
 		<i class="fas fa-plus fa-fw"></i> &nbsp; NUEVO USUARIO
 	</h3>
 	<p class="text-justify">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nostrum rerum animi natus beatae ex. Culpa blanditiis tempore amet alias placeat, obcaecati quaerat ullam, sunt est, odio aut veniam ratione.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nostrum rerum animi natus beatae ex. Culpa
+		blanditiis tempore amet alias placeat, obcaecati quaerat ullam, sunt est, odio aut veniam ratione.
 	</p>
 </div>
 
 <div class="container-fluid">
 	<ul class="full-box list-unstyled page-nav-tabs">
 		<li>
-			<a class="active" href="<?php echo SERVERURL;?>user-new/"><i class="fas fa-plus fa-fw"></i> &nbsp; NUEVO USUARIO</a>
+			<a class="active" href="<?php echo SERVERURL; ?>user-new/"><i class="fas fa-plus fa-fw"></i> &nbsp; NUEVO
+				USUARIO</a>
 		</li>
 		<li>
-			<a href="<?php echo SERVERURL;?>user-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE USUARIOS</a>
+			<a href="<?php echo SERVERURL; ?>user-list/"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; LISTA DE
+				USUARIOS</a>
 		</li>
 		<li>
-			<a href="<?php echo SERVERURL;?>user-search/"><i class="fas fa-search fa-fw"></i> &nbsp; BUSCAR USUARIO</a>
+			<a href="<?php echo SERVERURL; ?>user-search/"><i class="fas fa-search fa-fw"></i> &nbsp; BUSCAR USUARIO</a>
 		</li>
-	</ul>	
+	</ul>
 </div>
 
 <div class="container-fluid">
-	<form  class="form-neon FormularioAjax" action="" method="POST" data-form="save" autocomplete="off">
+	<form class="form-neon FormularioAjax" action="<?php echo SERVERURL; ?>" method="POST" data-form="save"
+		autocomplete="off">
 		<fieldset>
 			<legend><i class="far fa-address-card"></i> &nbsp; Información personal</legend>
 			<div class="container-fluid">
@@ -30,32 +34,37 @@
 					<div class="col-12 col-md-4">
 						<div class="form-group">
 							<label for="usuario_dni" class="bmd-label-floating">DNI</label>
-							<input type="text" pattern="[0-9-]{1,20}" class="form-control" name="usuario_dni_reg" id="usuario_dni" maxlength="20">
+							<input type="text" pattern="[0-9-]{1,20}" class="form-control" name="usuario_dni_reg"
+								id="usuario_dni" maxlength="20">
 						</div>
 					</div>
-					
+
 					<div class="col-12 col-md-4">
 						<div class="form-group">
 							<label for="usuario_nombre" class="bmd-label-floating">Nombres</label>
-							<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_nombre_reg" id="usuario_nombre" maxlength="35">
+							<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control"
+								name="usuario_nombre_reg" id="usuario_nombre" maxlength="35">
 						</div>
 					</div>
 					<div class="col-12 col-md-4">
 						<div class="form-group">
 							<label for="usuario_apellido" class="bmd-label-floating">Apellidos</label>
-							<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control" name="usuario_apellido_reg" id="usuario_apellido" maxlength="35">
+							<input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,35}" class="form-control"
+								name="usuario_apellido_reg" id="usuario_apellido" maxlength="35">
 						</div>
 					</div>
 					<div class="col-12 col-md-6">
 						<div class="form-group">
 							<label for="usuario_telefono" class="bmd-label-floating">Teléfono</label>
-							<input type="text" pattern="[0-9()+]{8,20}" class="form-control" name="usuario_telefono_reg" id="usuario_telefono" maxlength="20">
+							<input type="text" pattern="[0-9()+]{8,20}" class="form-control" name="usuario_telefono_reg"
+								id="usuario_telefono" maxlength="20">
 						</div>
 					</div>
 					<div class="col-12 col-md-6">
 						<div class="form-group">
 							<label for="usuario_direccion" class="bmd-label-floating">Dirección</label>
-							<input type="text" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}" class="form-control" name="usuario_direccion_reg" id="usuario_direccion" maxlength="190">
+							<input type="text" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ().,#\- ]{1,190}" class="form-control"
+								name="usuario_direccion_reg" id="usuario_direccion" maxlength="190">
 						</div>
 					</div>
 				</div>
@@ -69,25 +78,29 @@
 					<div class="col-12 col-md-6">
 						<div class="form-group">
 							<label for="usuario_usuario" class="bmd-label-floating">Nombre de usuario</label>
-							<input type="text" pattern="[a-zA-Z0-9]{1,35}" class="form-control" name="usuario_usuario_reg" id="usuario_usuario" maxlength="35">
+							<input type="text" pattern="[a-zA-Z0-9]{1,35}" class="form-control"
+								name="usuario_usuario_reg" id="usuario_usuario" maxlength="35">
 						</div>
 					</div>
 					<div class="col-12 col-md-6">
 						<div class="form-group">
 							<label for="usuario_email" class="bmd-label-floating">Email</label>
-							<input type="email" class="form-control" name="usuario_email_reg" id="usuario_email" maxlength="70">
+							<input type="email" class="form-control" name="usuario_email_reg" id="usuario_email"
+								maxlength="70">
 						</div>
 					</div>
 					<div class="col-12 col-md-6">
 						<div class="form-group">
 							<label for="usuario_clave_1" class="bmd-label-floating">Contraseña</label>
-							<input type="password" class="form-control" name="usuario_clave_1_reg" id="usuario_clave_1" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required="" >
+							<input type="password" class="form-control" name="usuario_clave_1_reg" id="usuario_clave_1"
+								pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required="">
 						</div>
 					</div>
 					<div class="col-12 col-md-6">
 						<div class="form-group">
 							<label for="usuario_clave_2" class="bmd-label-floating">Repetir contraseña</label>
-							<input type="password" class="form-control" name="usuario_clave_2_reg" id="usuario_clave_2" pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required="" >
+							<input type="password" class="form-control" name="usuario_clave_2_reg" id="usuario_clave_2"
+								pattern="[a-zA-Z0-9$@.-]{7,100}" maxlength="100" required="">
 						</div>
 					</div>
 				</div>
@@ -99,7 +112,8 @@
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-12">
-						<p><span class="badge badge-info">Control total</span> Permisos para registrar, actualizar y eliminar</p>
+						<p><span class="badge badge-info">Control total</span> Permisos para registrar, actualizar y
+							eliminar</p>
 						<p><span class="badge badge-success">Edición</span> Permisos para registrar y actualizar</p>
 						<p><span class="badge badge-dark">Registrar</span> Solo permisos para registrar</p>
 						<div class="form-group">
@@ -115,9 +129,11 @@
 			</div>
 		</fieldset>
 		<p class="text-center" style="margin-top: 40px;">
-			<button type="reset" class="btn btn-raised btn-secondary btn-sm"><i class="fas fa-paint-roller"></i> &nbsp; LIMPIAR</button>
+			<button type="reset" class="btn btn-raised btn-secondary btn-sm"><i class="fas fa-paint-roller"></i> &nbsp;
+				LIMPIAR</button>
 			&nbsp; &nbsp;
-			<button type="submit" class="btn btn-raised btn-info btn-sm"><i class="far fa-save"></i> &nbsp; GUARDAR</button>
+			<button type="submit" class="btn btn-raised btn-info btn-sm"><i class="far fa-save"></i> &nbsp;
+				GUARDAR</button>
 		</p>
 	</form>
 </div>
